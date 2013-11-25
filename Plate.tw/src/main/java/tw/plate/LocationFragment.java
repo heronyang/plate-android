3928e693ab84809b1c1d3731a4ef6b72e52cdd6a
Anthony Ang <anthonyang86@gmail.com>
2013-11-25 22:46:04 +0800

    bug in RestaurantActivity

--- Plate.tw/src/main/java/tw/plate/LocationFragment.java
+++ Plate.tw/src/main/java/tw/plate/LocationFragment.java
@@ -1,10 +1,12 @@
 package tw.plate;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -23,20 +25,26 @@ public class LocationFragment extends Fragment{
 
         //Displaying ListView items
         ArrayAdapter<String> files = new ArrayAdapter(getActivity(),
-                android.R.layout.simple_list_item_1, constants.CANTEEN_LIST);
+                android.R.layout.simple_list_item_1, Constants.CANTEEN_LIST);
 
         ListView lv = (ListView) v.findViewById(R.id.lv_canteen);
         lv.setAdapter(files);
 
         //OnClickListener for each dinning hall
-
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Intent restActivity = new Intent(view.getContext(),RestaurantActivity.class);
+                startActivity(restActivity);
+            }
+        });
 
         return v;
     }
 
 
     public static LocationFragment newInstance(String text){
-
         LocationFragment locationFragment = new LocationFragment();
         Bundle b = new Bundle();
         b.putString("msg",text);
