156d25e265aaa9874ee16b82b68766977f725797
Anthony Ang <anthonyang86@gmail.com>
2013-11-25 21:19:11 +0800

    added new java file

---
+++ Plate.tw/src/main/java/tw/plate/constants.java
@@ -0,0 +1,11 @@
+package tw.plate;
+
+/**
+ * Created by Hung on 11/25/13.
+ */
+final public class constants {
+    public static final String[] CANTEEN_LIST= new String[]{"第一餐廳", "第二餐廳", "女二餐廳", "其他"};
+    public static final String API_URI_PREFIX = "http://localhost:8000";
+
+    private constants(){}
+}
