9c838a1aef900d709549b4a0b1a47a70477e0d64
Heron Yang <v6510young88@gmail.com>
2013-12-03 03:19:20 +0800

    improvement on confirm-order page

--- Plate.tw/src/main/java/tw/plate/ConfirmOrder.java
+++ Plate.tw/src/main/java/tw/plate/ConfirmOrder.java
@@ -1,6 +1,9 @@
 package tw.plate;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.preference.DialogPreference;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
@@ -13,24 +16,23 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class ConfirmOrder extends ActionBarActivity {
-    //TEST Data Only
-    String serverTime = "11:30";
-    String restName = "元氣";
-    String myOrder;
-    String popupWarning = "This is popup warning \nThis is popup warning \nThis is popup warning \nThis is popup warning \nThis is popup warning \nThis is popup warning \n";
+
     //OrderList passedOrderList;
     private ArrayList<String> mealNames = new ArrayList<String>();
     private ArrayList<Integer> mealPrices = new ArrayList<Integer>()
             , mealID = new ArrayList<Integer>()
             , mealAmount = new ArrayList<Integer>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,30 +40,50 @@ public class ConfirmOrder extends ActionBarActivity {
         setContentView(R.layout.activity_confirm_order);
 
         Intent intent = getIntent();
-        Log.d(Constants.LOG_TAG,"Confirm order Activity");
-        //TODO Receive the data getStringArrayExtra
-        //Bundle data = getIntent().getExtras();
-        //passedOrderList = (OrderList) data.getParcelable("order");
-        //Log.d(Constants.LOG_TAG,"Passed order at postition 0: Meal Name: "+passedOrderList.getMealOrderList(0).meal_name +"Amount: "+passedOrderList.getAmountOrderList(0) +"pieces");
+
         mealNames = intent.getStringArrayListExtra("orderMealNames");
         mealPrices = intent.getIntegerArrayListExtra("orderMealPrice");
         mealID = intent.getIntegerArrayListExtra("orderMealID");
         mealAmount = intent.getIntegerArrayListExtra("orderMealAmount");
 
-        for(int i=0; i<mealNames.size(); i++)
-            Log.d(Constants.LOG_TAG,"Passed order at postition 0: Meal Name: "+mealNames.get(i) +"Amount: "+ mealAmount.get(i) +"pieces" );
+        intent.removeExtra("orderMealNames");
+        intent.removeExtra("orderMealPrice");
+        intent.removeExtra("orderMealID");
+        intent.removeExtra("orderMealAmount");
 
-        TextView tv_time = (TextView) findViewById(R.id.tv_time);
-        TextView tv_rest = (TextView) findViewById(R.id.tv_rest);
-        TextView tv_myOrder = (TextView) findViewById(R.id.tv_your_order);
+        //
+        for(int i=0; i<mealNames.size(); i++)
+            Log.d(Constants.LOG_TAG,"Passed order at position 0: Meal Name: "
+                    + mealNames.get(i) + "Amount: " + mealAmount.get(i) +"pieces" );
+
+        listviewAndTotalAmountSetup();
+        buttonSetup();
+    }
+
+    private void listviewAndTotalAmountSetup() {
+
+        int totalAmount = 0;
+
+        ListView lv = (ListView) findViewById(android.R.id.list);
+        int l = mealNames.size(), i;
+        String [] rowData = new String[l];
+        for( i=0 ; i<l ; i++) {
+            rowData[i] = mealNames.get(i) + " " + mealPrices.get(i) + "NTD * " + mealAmount.get(i);
+            totalAmount += mealPrices.get(i) * mealAmount.get(i);
+        }
+
+        ArrayAdapter<String> adapter;
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, rowData);
+        lv.setAdapter(adapter);
+
+        TextView tv = (TextView)findViewById(R.id.tvTotalAmount);
+        tv.setText(totalAmount + " NTD");
+    }
+
+    private void buttonSetup() {
+
         Button btCancel = (Button) findViewById(R.id.bt_cancel);
         Button btOk = (Button) findViewById(R.id.bt_ok);
-
-        tv_time.setText(serverTime);
-        tv_rest.setText(restName);
-        //TODO create a complex-string from given data
-        myOrder = "my order";
-        tv_myOrder.setText(myOrder);
 
         btCancel.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,37 +91,37 @@ public class ConfirmOrder extends ActionBarActivity {
                 finish();
             }
         });
+
         btOk.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LayoutInflater inflater = (LayoutInflater) getBaseContext()
-                        .getSystemService(LAYOUT_INFLATER_SERVICE);
-                View popupView = inflater.inflate(R.layout.popup, null);
-                final PopupWindow popupWind = new PopupWindow(popupView, android.app.ActionBar
-                        .LayoutParams.WRAP_CONTENT, android.app.ActionBar.LayoutParams.WRAP_CONTENT);
-                TextView popupMessage = (TextView)  popupView.findViewById(R.id.tv_pop);
-                popupMessage.setText(popupWarning);
-                Button yes = (Button) popupView.findViewById(R.id.bt_pop_yes);
-                Button no = (Button) popupView.findViewById(R.id.bt_pop_no);
-                no.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        popupWind.dismiss();
-                    }
-                });
-                yes.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        //FIXME to the next fragment of mainactivity
-
-                    }
-                });
-                //TODO Fix the popup xml and make the background darker
-                popupWind.showAtLocation(view, Gravity.CENTER, 0, 0);
-                popupWind.update(0, 0, 300, 300);
-
+                popupDoubleConfirmMessage();
             }
         });
+    }
+
+    private void popupDoubleConfirmMessage () {
+        AlertDialog.Builder builder = new AlertDialog.Builder(ConfirmOrder.this);
+
+        builder.setMessage(R.string.confirm_order_warning_message)
+                .setTitle(R.string.confirm_order_warning_title);
+
+        builder.setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+                Intent mainActivityIntent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(mainActivityIntent);
+            }
+        });
+
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // If cancel, do nothing
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 
 
@@ -121,22 +143,6 @@ public class ConfirmOrder extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_confirm_order, container, false);
-            return rootView;
-        }
     }
 
 }
