5c6030c18d3fca88df7cabaa6fb68b306cad20da
Heron Yang <v6510young88@gmail.com>
2014-10-07 00:36:50 +0800

    before pushing to github

--- Plate.tw/src/main/java/tw/plate/Constants.java
+++ Plate.tw/src/main/java/tw/plate/Constants.java
@@ -7,7 +7,7 @@ final public class Constants {
 
     /* Names */
     public static final String[] CANTEEN_LIST = new String[]{"女 二 餐 廳", "第 二 餐 廳", "第 一 餐 廳", "其  他"};
-    public static final String API_URI_PREFIX = DEBUG_MODE?"http://192.168.1.228:8080":"https://api.plate.tw"; // Release Mode
+    public static final String API_URI_PREFIX = DEBUG_MODE?"http://192.168.0.121:8080":"https://api.plate.tw"; // Release Mode
 
     //public static final String API_URI_PREFIX = "http://10.0.2.2:8000"; // Developing Mode
     //public static final String API_URI_PREFIX = "http://192.168.0.121:8080"; // Heron's Local Developing Mode
